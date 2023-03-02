ed50789882c6d9aa7e578d2c4c9a2e81f8eda5bf
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2023-03-02 13:22:41 -0800

    tests

--- index.test.js
+++ index.test.js
@@ -17,3 +17,18 @@ test('test runs with expanded values', () => {
     const dotenv_action = require('./dotenv_action');
     expect(dotenv_action("fixtures/.expand.env")).toEqual({ fixtures_3: "xyz", "expanded": "123-xyz", "expanded_2": "123-xyz" });
 })
+
+test('test runs with bypass case for keys', () => {
+    const dotenv_action = require('./dotenv_action');
+    expect(dotenv_action("fixtures/.case.env", "bypass")).toEqual({ "FIXTURES_4": "foo", "fixtures_5": "bar", "Fixtures_6": "abc" });
+})
+
+test('test runs with lower case for keys', () => {
+    const dotenv_action = require('./dotenv_action');
+    expect(dotenv_action("fixtures/.case.env", "lower")).toEqual({ "fixtures_4": "foo", "fixtures_5": "bar", "fixtures_6": "abc" });
+})
+
+test('test runs with upper case for keys', () => {
+    const dotenv_action = require('./dotenv_action');
+    expect(dotenv_action("fixtures/.case.env", "upper")).toEqual({ "FIXTURES_4": "foo", "FIXTURES_5": "bar", "FIXTURES_6": "abc" });
+})
